8b518690f5ebb44b94037794f7868df7a753730f
Kamil <kamil-jamroz@o2.pl>
2023-09-27 16:50:42 +0200

    FIX: hidden playlist if filtering

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -52,26 +52,29 @@ const Page = async ({ searchParams }: Props) => {
           </div>
         </section>
       )}
-      {resourcePlaylist.map((item: any) => (
-        <section
-          key={item._id}
-          className="flex-center mt-6 w-full flex-col sm:mt-20"
-        >
-          <h1 className="heading3 self-start text-white-800">{item.title}</h1>
-          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-            {item.resources.map((resource: any) => (
-              <ResourceCard
-                key={resource._id}
-                title={resource.title}
-                id={resource._id}
-                image={resource.image}
-                downloadNumber={resource.views}
-                downloadLink={resource.downloadLink}
-              />
-            ))}
-          </div>
-        </section>
-      ))}
+
+      {searchParams?.query == undefined &&
+        searchParams?.category == undefined &&
+        resourcePlaylist.map((item: any) => (
+          <section
+            key={item._id}
+            className="flex-center mt-6 w-full flex-col sm:mt-20"
+          >
+            <h2 className="heading3 self-start text-white-800">{item.title}</h2>
+            <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+              {item.resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
+                />
+              ))}
+            </div>
+          </section>
+        ))}
     </main>
   );
 };
