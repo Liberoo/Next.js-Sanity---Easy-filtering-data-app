f684285136bcecdaab58ea935d6fef02c1629259
Kamil <kamil-jamroz@o2.pl>
2023-09-27 15:50:03 +0200

    Improve metadata, fix skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -4,14 +4,14 @@ const Loading = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col ">
       <section className="nav-padding w-full">
-        <Skeleton className="w-full h-[274px] rounded-lg bg-red-100 " />
+        <Skeleton className="w-full h-[274px] rounded-lg bg-black-200 " />
       </section>
       <section className="mt-7 flex w-full flex-col sm:mt-20">
-        <Skeleton className="h-10 w-56 bg-red-100" />
+        <Skeleton className="h-10 w-56 bg-black-200" />
         <div className="mt-12 flex- w-full flex-wrap justify-center gap-16 sm:justify-start">
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-red-100" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-red-100" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-red-100" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200" />
         </div>
       </section>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ease Filter App",
   description: "Filtering ease next-13 app",
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twitter:image": "https://i.ibb.com/d6TXxB2/homepage-thumbnail.jpg",
+    "twitter:card": "summary_large_image",
+    "og:url": "kamiljamroz.pl",
+    "og:image": "https://i.ibb.com/d6TXxB2/homepage-thumbnail.jpg",
+    "og:type": "website",
+  },
 };
 
 export default function RootLayout({
